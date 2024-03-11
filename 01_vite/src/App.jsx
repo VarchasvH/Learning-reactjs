0225296ae383994d7e77882048d2a58cce51674a
Varchasv Hoon <100064742+VarchasvH@users.noreply.github.com>
2024-03-11 15:57:35 +0000

    some other things

---
+++ 01_vite/src/App.jsx
@@ -0,0 +1,14 @@
+import Chai from './chai';
+function App() {
+ // JSX can only return 1 element but it can have multiple elements inside that one element.
+  return (
+    <>
+    <Chai/>
+    <h1>This is a heading</h1>
+    <p>Lorem ipsum dolor sit amet.</p>
+    </>
+    
+  )
+}
+
+export default App
--- 01_vite_react/src/App.jsx
+++
@@ -1,8 +0,0 @@
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <h1>React Project | Varchasv Hoon</h1>
-  )
-}
-export default App
