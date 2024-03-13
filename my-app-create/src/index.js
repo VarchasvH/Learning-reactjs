52e60b02ae50f06afbb2b0f9d73d48cdafa094e4
Varchasv Hoon <100064742+VarchasvH@users.noreply.github.com>
2024-03-13 12:32:53 +0000

    moving onto best seller books

--- my-app-create/src/index.js
+++ my-app-create/src/index.js
@@ -1,47 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-/* First Component 
-const Greetings = () => {
-  return <h2> This is the first component</h2>;
-};
-export default Greetings;
-*/
-
-/* Second component using jsx
-const Greetings = () => {
-  return React.createElement("h2", {}, "Second Component");
-};
-*/
-
-/*
-const Greetings = () => {
-  return (
-    <div>
-      <h2>My Third Component</h2>
-    </div>
-  );
-};
-export default Greetings;
-*/
-
 function Greetings() {
   return (
-    <>
-      <div>
-        <h3>
-          <ul>
-            <li>
-              <a href="https://google.com">hello-world</a>
-            </li>
-          </ul>
-        </h3>
-      </div>
-    </>
+    <div>
+      <Person />
+      <Message />
+    </div>
   );
 }
 
-export default Greetings;
+const Person = () => <h2>john doe</h2>;
+const Message = () => {
+  return <p>this is my message</p>;
+};
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 root.render(<Greetings />);
